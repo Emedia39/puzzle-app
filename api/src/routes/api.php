562d4950e1cd46ed3ce6206fa0e895f9f8b02253
Emedia39 <241207@st.yoshida-g.ac.jp>
2025-11-27 15:12:22 +0900

    初期

--- api/src/routes/api.php
+++ api/src/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,6 +9,7 @@ use App\Http\Controllers\StageController;
 use App\Http\Controllers\ExpController;
 
 use App\Http\Controllers\UserDegreeController;
+
 
 //users/items
 Route::get('users/index', [UserController::class, 'index'])->name('users.index');
@@ -35,3 +37,6 @@ Route::get('exps/{exp_id}', [ExpController::class, 'show'])->name('exps.show');
 //user_degree/
 Route::get('user_degrees/index', [UserDegreeController::class, 'index'])->name('user_degrees.index');
 Route::get('user_degrees/{user_id}', [UserDegreeController::class, 'show'])->name('user_degrees.show');
+
+
+Route::get('tests', [TestController::class, 'test']);
